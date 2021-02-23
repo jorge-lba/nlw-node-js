76efa51c251ffa5313dcc2957609bc32d6643d46
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-02-23 00:37:36 -0300

    Aula 1

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World - NLW04" })
+})
+
+app.post("/", (req, res) => {
+  return res.json({ message: "Os dados foram salvos com sucesso!" })
+})
+
+app.listen(3333, () => {
+  console.log("Server on, PORT 3333")
+})
