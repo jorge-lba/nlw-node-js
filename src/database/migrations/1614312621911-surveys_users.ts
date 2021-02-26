477356e0d63534285704904134a8bf4c2ebe3835
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-02-26 01:10:42 -0300

    Aula 3

---
+++ src/database/migrations/1614312621911-surveys_users.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class surveysUsers1614312621911 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
